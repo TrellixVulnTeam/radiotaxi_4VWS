7a14362288a96325759cf1e2f572206bcdbd1730
rjarpa <rjarpa@gmail.com>
2018-06-06 23:28:23 -0400

    branch entities

--- users/models.py
+++ users/models.py
@@ -23,43 +23,133 @@ class User(models.Model):
     def __str__(self):
         return self.title
 
-#class Request(models.Model):
+class Trip(models.Model):
 
 #    id=models.Aggregate
 #    passanger==models.CharField
 
 
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    created_by =models.CharField(max_length=20)
+
+
     def __str__(self):
         return title
 
-#class Employee(models.Model):
+class Employee(models.Model):
 
-#class Driver(models.Models):
-#        def __str__(self):
-#            return title
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    created_by =models.CharField(max_length=20)
+
+
+    def __str__(self):
+        return title
+
+
+class Driver(models.Models):
+
+    first_name=models.CharField(max_length=100)
+    last_name=models.CharField(max_length=100)
+    national_id=models.CharField(max_length=20)
+
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    created_by =models.CharField(max_length=20)
+
+
+    def __str__(self):
+        return title
 
 #class Billing(models.Model):
 
-#class Branches(models.Model):
+class Aggrement(models.Model):
 
-#class AddressLists(models.Model)
+    customer=models.ForeignKey(Customers,on_delete=models.CASCADE)
+    def __str__(self):
+        return self.title
 
 
+class Branches(models.Model):
+
+    def __str__(self):
+        return self.title
+
+
+
+class Contacts(models.Model):
+
+
+    customer=models.ForeignKey(Customers,on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    created_by =models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.title
+
+
+class AddressLists(models.Model)
+
+    is_favorite = CharField(max_length)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    created_by =models.CharField(max_length=20)
+
+
+
+    def __str__(self):
+        return self.title
+
+class Passenger(models.Model):
+    username=models.CharField(max_length=25)
+    first_name=model.CharField(max_length=100)
+    last_name=models.CharField(max_length=50)
+    gender=models.IntegerField()
+    date_birth=models.DateField()
+    email=models.EmailField()
+
+    customer=models.ForeignKey(Customers,on_delete=models.CASCADE)
+
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    created_by =models.CharField(max_length=20)
+
+
+    def __str__(self):
+        return self.title
+
+
+class Special_Fields(models.Model):
+
+
+    customer=models.ForeignKey(Customers,on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    created_by =models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.title
 
 class Customers(models.Model):
 
-    name=models.CharField(max_length=150)
-    vat_code=models.CharField(max_length=20)
-    code=models.IntegerField(null=False)
-    address01= models.CharField(max_length=100)
-    address02=models.CharField(max_length=100)
-    address03 =models.CharField(max_length=100)
+    name = models.CharField(max_length=150)
+    vat_code = models.CharField(max_length=20)
+    code = models.IntegerField(null=False)
+    address01 = models.CharField(max_length=100)
+    address02 = models.CharField(max_length=100)
+    address03 = models.CharField(max_length=100)
     state = models.CharField(max_length=50)
     city = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
     created_by =models.CharField(max_length=20)
-    country= models.CharField(max_length=25)
+    country = models.CharField(max_length=25)
 
 
 
