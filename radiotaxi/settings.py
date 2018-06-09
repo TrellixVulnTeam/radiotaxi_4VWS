246e21c44835828d21fddb48811c9fec91596240
Alexis Loyola <alexis.loyola@gmail.com>
2018-06-09 14:38:58 -0400

    Normalización de settings

--- radiotaxi/settings.py
+++ radiotaxi/settings.py
@@ -79,19 +79,15 @@ WSGI_APPLICATION = 'radiotaxi.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    },
-    'mysql':{
+    'default':{
         'NAME': 'radiotaxi',
         'ENGINE': 'django.db.backends.mysql',
-        'USER': 'root',
-        'PASSWORD': 'Atlas.,2038',
+        'USER': 'devmaster',
+        'PASSWORD': 'itwillruletheworld',
+        'HOST': 'dev.oceanictp.cl',
         'OPTIONS': {
           'autocommit': True,
         },
-
     }
 }
 
